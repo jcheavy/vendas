a6fcd0b91118ac793300f5cdf0be232a67586d74
jcheavy <jcheavy@hotmail.com>
2020-09-11 23:57:52 -0300

    ateração telaPrincipal

--- src/views/TelaPrincipal.java
+++ src/views/TelaPrincipal.java
@@ -3,6 +3,9 @@ package views;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 
@@ -15,40 +18,50 @@ import util.Paleta;
 public class TelaPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+	private JPanel panelTop;
+	private JPanel panelEsq;
+	private JPanel panelData;
 
 	private void resolucao() {
 		Toolkit t = Toolkit.getDefaultToolkit();
 		Dimension dimension = t.getScreenSize();
 		setSize((dimension.width + 5), (dimension.height - 38));
-	}	
-    
+	}
+
 	public TelaPrincipal() {
-         
+
 		Container telaPri = getContentPane();
 		telaPri.setBackground(Paleta.cor_6);
 		telaPri.setLayout(new BorderLayout());
+
+		panelTop = new JPanel(new GridBagLayout());
+		panelTop.setPreferredSize(new Dimension(30, 100));
+		panelTop.setBackground(Paleta.cor_4);
+		telaPri.add(panelTop, BorderLayout.NORTH);
+
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.anchor = GridBagConstraints.WEST;
+		constraints.insets = new Insets(10, 10, 10, 10);
+
+		panelData = new JPanel();
+		panelData.setPreferredSize(new Dimension(100, 40));
 		
-		
-		JPanel panelTop = new JPanel();	
-		panelTop.setPreferredSize(new Dimension(30,100));
-	    panelTop.setBackground(Paleta.cor_4); 
-	    telaPri.add(panelTop, BorderLayout.NORTH);
-	    
-	    
-	    
-	    JPanel panelEsq = new JPanel();
-		panelEsq.setPreferredSize(new Dimension(250,100));
+		// add components to the panel
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		panelTop.add(panelData, constraints);
+
+		panelEsq = new JPanel();
+		panelEsq.setPreferredSize(new Dimension(250, 100));
 		panelEsq.setBackground(Paleta.cor_2);
 		telaPri.add(panelEsq, BorderLayout.WEST);
-		
-		
-		setResizable(true);	
+
+		setResizable(true);
 		resolucao();
 	}
 
 	public static void main(String[] args) {
-		
+
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
