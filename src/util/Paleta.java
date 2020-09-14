eba114526b0df0f97f252989433c10b806d0362c
jcheavy <jcheavy@hotmail.com>
2020-09-14 19:35:08 -0300

    coments e cor na telaPrincipal

--- src/util/Paleta.java
+++ src/util/Paleta.java
@@ -8,12 +8,35 @@ public class Paleta {
 	 * Paleta de Cores
 	 * 
 	 */
-
+     
+	/**
+	 * Preto
+	 */
 	public static Color cor_1 = new Color(23, 30, 38);
+	
+	/**
+	 * Cinza escuro
+	 */
 	public static Color cor_2 = new Color(47, 53, 59);
+	
+	/**
+	 * Azul escuro
+	 */
 	public static Color cor_3 = new Color(33, 66, 88);
+	
+	/**
+	 * Azul
+	 */
 	public static Color cor_4 = new Color(13, 87, 136);
+	
+	/**
+	 * Cinza padão
+	 */
 	public static Color cor_5 = new Color(239, 239, 239);
+	
+	/**
+	 * Preto
+	 */
 	public static Color cor_6 = new Color(35, 45, 53);
 
 }
--- src/views/TelaPrincipal.java
+++ src/views/TelaPrincipal.java
@@ -1,16 +1,19 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.LayoutManager;
+import java.awt.TextField;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import util.Paleta;
@@ -20,7 +23,9 @@ public class TelaPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel panelTop;
 	private JPanel panelEsq;
-	private JPanel panelData;
+	private JPanel panelData, panelPesquisa;
+	private JButton btpesquisar;
+	private JTextField txpesquisar;
 
 	private void resolucao() {
 		Toolkit t = Toolkit.getDefaultToolkit();
@@ -34,22 +39,41 @@ public class TelaPrincipal extends JFrame {
 		telaPri.setBackground(Paleta.cor_6);
 		telaPri.setLayout(new BorderLayout());
 
-		panelTop = new JPanel(new GridBagLayout());
-		panelTop.setPreferredSize(new Dimension(30, 100));
+		panelTop = new JPanel(new BorderLayout());
+		panelTop.setPreferredSize(new Dimension(30, 40));
 		panelTop.setBackground(Paleta.cor_4);
 		telaPri.add(panelTop, BorderLayout.NORTH);
 
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.anchor = GridBagConstraints.WEST;
-		constraints.insets = new Insets(10, 10, 10, 10);
+		constraints.insets = new Insets(3, 1, 3, 1);
 
 		panelData = new JPanel();
-		panelData.setPreferredSize(new Dimension(100, 40));
+		panelData.setBackground(Paleta.cor_4);
+		panelData.setPreferredSize(new Dimension(250, 100));		
+		panelTop.add(panelData, BorderLayout.WEST);
 		
-		// add components to the panel
+
+		panelPesquisa = new JPanel(new GridBagLayout());
+		panelPesquisa.setBackground(Paleta.cor_4);
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		panelTop.add(panelPesquisa, BorderLayout.CENTER);
+	
+		
+		
+		btpesquisar = new JButton("Pesquisar");
+		txpesquisar = new JTextField(10);
+
+	
 		constraints.gridx = 0;
 		constraints.gridy = 0;
-		panelTop.add(panelData, constraints);
+		panelPesquisa.add(btpesquisar, constraints);
+
+		constraints.fill = GridBagConstraints.VERTICAL;
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		panelPesquisa.add(txpesquisar, constraints);
 
 		panelEsq = new JPanel();
 		panelEsq.setPreferredSize(new Dimension(250, 100));
